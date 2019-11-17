0d7012e8b9126367b754289e0f9badeed10cf21c
Talip ÇAKIR <32384299+talipcakir@users.noreply.github.com>
2019-11-17 22:51:02 +0300

    Add files via upload

---
+++ HC_SR04_LCD_I2C.ino
@@ -0,0 +1,53 @@
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>  // I2C kütüphanesini verdiğim linkten indirip   include edebilirsiniz
+
+// Define Trig and Echo pin:
+#define trigPin 2
+#define echoPin 3
+// Define variables:
+long duration;
+int distance;
+
+LiquidCrystal_I2C lcd(0x27,16,2); 
+
+void setup()
+{
+ lcd.begin();                      // LCD yi başlatma
+  // Define inputs and outputs:
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  //Begin Serial communication at a baudrate of 9600: (for sensor control)
+  Serial.begin(9600);
+
+}
+void loop()
+{
+   lcd.backlight();     // lcd nin arka ışıgını açar
+   lcd.setCursor(0,0);
+   lcd.print("Start System!");
+   lcd.setCursor(0,1);
+   lcd.print("Sensor is Ready...");
+  // Clear the trigPin by setting it LOW:
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  // Trigger the sensor by setting the trigPin high for 10 microseconds:
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Read the echoPin, pulseIn() returns the duration (length of the pulse) in microseconds:
+  duration = pulseIn(echoPin, HIGH);
+  // Calculate the distance:
+  distance= duration*0.034/2;
+  // Print the distance on the Serial Monitor (Ctrl+Shift+M):
+  Serial.print("Distance = ");
+  Serial.print(distance);
+  Serial.println(" cm");
+  lcd.clear();
+   lcd.setCursor(0,0);
+   lcd.print("Distance:");
+   lcd.setCursor(0,1);
+   lcd.print(distance);
+   lcd.print("cm");
+   
+  delay(4000);
+}
