b407eb7f33ca2e0092d20dfc7885235b1420bef4
Talip ÇAKIR <32384299+talipcakir@users.noreply.github.com>
2019-11-17 22:56:19 +0300

    Update HC_SR04_LCD_I2C.ino

--- HC_SR04_LCD_I2C.ino
+++ HC_SR04_LCD_I2C.ino
@@ -27,6 +27,7 @@ void loop()
    lcd.print("Start System!");
    lcd.setCursor(0,1);
    lcd.print("Sensor is Ready...");
+   delay(4000);
   // Clear the trigPin by setting it LOW:
   digitalWrite(trigPin, LOW);
   delayMicroseconds(5);
@@ -37,17 +38,17 @@ void loop()
   // Read the echoPin, pulseIn() returns the duration (length of the pulse) in microseconds:
   duration = pulseIn(echoPin, HIGH);
   // Calculate the distance:
-  distance= duration*0.034/2;
+  distance= duration*0.034/2; //for cm.
   // Print the distance on the Serial Monitor (Ctrl+Shift+M):
   Serial.print("Distance = ");
   Serial.print(distance);
   Serial.println(" cm");
   lcd.clear();
-   lcd.setCursor(0,0);
-   lcd.print("Distance:");
-   lcd.setCursor(0,1);
-   lcd.print(distance);
-   lcd.print("cm");
+  lcd.setCursor(0,0);
+  lcd.print("Distance:");
+  lcd.setCursor(0,1);
+  lcd.print(distance);
+  lcd.print("cm");
    
-  delay(4000);
+  delay(4000); //stop time
 }
