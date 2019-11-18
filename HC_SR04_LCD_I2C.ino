5e2f94dd8496f4426fc6074dd3d5e97d9bf442dd
Talip ÇAKIR <32384299+talipcakir@users.noreply.github.com>
2019-11-18 08:26:52 +0300

    Update HC_SR04_LCD_I2C.ino

--- HC_SR04_LCD_I2C.ino
+++ HC_SR04_LCD_I2C.ino
@@ -1,3 +1,21 @@
+//In this project, the distance data from the hc-sr04 sensor is sent to the LCD screen as output.
+//Arguments used:
+//-HC-SR04 sensor
+//-16 * 2 LCD (I2C module integrated)
+// (Module Pin = Arduino Pin)
+//HC-SR sensor:
+//  VCC = 5v
+//  Trig = 3 (Digital)
+//  Echo = 2 (Digital)
+//  GND = GND
+//  
+//I2C Module:
+//  GND = GND
+//  VCC = 5v
+//  SDA = A4 (Analog)
+//  SDL = A5 (Analog)
+
+//If you want 'Serial' code at the beginning of the '//' signs can be removed from the serial monitor data.
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>  // I2C lib
 
@@ -16,18 +34,17 @@ void setup()
   // Define inputs and outputs:
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
-  //Begin Serial communication at a baudrate of 9600: (for sensor control)
-  Serial.begin(9600);
-
-}
-void loop()
-{
+  //Begin Serial communication at a baudrate of 9600:
+  //Serial.begin(9600);
    lcd.backlight();
    lcd.setCursor(0,0);
    lcd.print("Start System!");
    lcd.setCursor(0,1);
    lcd.print("Sensor is Ready...");
    delay(4000);
+}
+void loop()
+{
   // Clear the trigPin by setting it LOW:
   digitalWrite(trigPin, LOW);
   delayMicroseconds(5);
@@ -40,15 +57,15 @@ void loop()
   // Calculate the distance:
   distance= duration*0.034/2; //for cm.
   // Print the distance on the Serial Monitor (Ctrl+Shift+M):
-  Serial.print("Distance = ");
-  Serial.print(distance);
-  Serial.println(" cm");
-  lcd.clear();
+  //Serial.print("Distance = ");
+  //Serial.print(distance);
+  //Serial.println(" cm");
+  lcd.clear(); //lcd screen clear
   lcd.setCursor(0,0);
   lcd.print("Distance:");
   lcd.setCursor(0,1);
   lcd.print(distance);
   lcd.print("cm");
    
-  delay(4000); //stop time
+  delay(100); //stop time
 }
